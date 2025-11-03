79c893a3357cc66e8e94ad72209b4f4aa69b484f
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2025-11-03 23:36:13 +0300

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? "/alfa-test" : "";
+
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: basePath,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
